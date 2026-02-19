b661b53a37a1dc340a4ce2adea811043199f6340
kartik maurya <kartikmorya860@gmail.com>
2026-02-19 22:01:57 +0530

    Delete lec_6.py

--- lec_6.py
+++
@@ -1,9 +0,0 @@
-# count the numbers 
-n = int(input("Enter the number :"))
-count = 0
-while n > 0:     
-                    count += 1
-                    n = n // 10
-                    print(count)
-                    
-
