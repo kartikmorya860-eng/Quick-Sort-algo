a11eeea78061c2785c7336464240f806388c815a
kartik maurya <kartikmorya860@gmail.com>
2026-02-19 21:59:42 +0530

    Delete lec_11.py

--- lec_11.py
+++
@@ -1,9 +0,0 @@
-n = [5,3,2,2,1,5,5,7,5,10]
-m =[10,111,1,9,5,67,2]
-count = 0
-for i in range (len(m)):
-                    count = 0
-                    for j in range (len(n)):
-                            if m[i] == n[j]:
-                                    count += 1
-                    print(count)
