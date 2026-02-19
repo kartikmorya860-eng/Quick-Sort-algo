b4d45a93936c19eaaadf5c130217fec32d748e44
kartik maurya <kartikmorya860@gmail.com>
2026-02-19 22:00:26 +0530

    Delete lec_13.py

--- lec_13.py
+++
@@ -1,8 +0,0 @@
-#recursion using parameter 
-# def para(x,n):
-#                     if (n == 0):
-#                             return
-#                     print(x)
-#                     para(x,n-1)
-# para(15,4)
-
